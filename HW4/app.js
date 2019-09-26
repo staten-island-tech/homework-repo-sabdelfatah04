42a298c74c0e95793eb73d0aac873bfd5d6b40db
Sarah Abdelfatah <sarah.abdelfatah@sitechhs.com>
2019-09-26 19:14:19 -0400

    jhb

---
+++ HW4/app.js
@@ -0,0 +1,27 @@
+// Global Score 
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test() {
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('Function Scope: ', a, b, c);
+// }
+// test();
+
+
+
+// if(true) {
+//     //Black Scope
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('If Scope: ', a, b, c);
+// }
+
+// for(let a = 0; a < 10; a++) { // if you use var instead of let changes to 10
+//     console.log(`Loop: ${a}`);
+// }
+console.log('Global Scope: ', a, b, c);
